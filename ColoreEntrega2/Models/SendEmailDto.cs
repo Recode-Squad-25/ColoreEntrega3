1e60097863f9195083c23e07c444219d1a31b89c
BrandaoSt <patriciabrandao7@gmail.com>
2022-02-11 16:18:39 -0300

    Tela de contato finalizada

---
+++ ColoreEntrega2/Models/SendEmailDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ColoreEntrega2.Models
+{
+    public class SendEmailDto
+    {
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Telefone { get; set; }
+        [Required]
+        public string? Assunto { get; set; }
+        [Required]
+        public string? Mensagem { get; set; }
+
+        public string? Body { get; set; }
+
+        public string? To { get; set; }
+
+        public string? From { get; set; }
+
+        public string? Subject { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
